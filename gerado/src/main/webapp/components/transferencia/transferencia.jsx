8ab1ca21a199a2f761b826738e34309e49df7d08
Pedro Hoppe <pedrohbh@users.noreply.github.com>
2022-11-22 21:09:20 -0300

    Atualização

---
+++ gerado/src/main/webapp/components/transferencia/transferencia.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export default function transferencia(props) {
+
+	
+	
+	render() {  
+      return (
+        <div></div>
+      );
+    }
+  }
+
+}

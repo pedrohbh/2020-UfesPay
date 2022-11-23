be6e3428e83e024ffe2a380b8cb163fd822e77a4
Pedro Hoppe <pedrohbh@users.noreply.github.com>
2022-11-22 21:03:53 -0300

    Geração

---
+++ gerado/src/main/webapp/components/layout/layout.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export default function layout(props) {
+
+	
+	
+	render() {  
+      return (
+        <div><div id="children">{{ children }}</div>
+	<navtop />
+	</div>
+      );
+    }
+  }
+
+}
